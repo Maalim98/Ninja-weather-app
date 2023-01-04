5df75ae0d36b088cc0bf60f7f719b7e06c774989
Maalim98 <ashrafzani585@gmail.com>
2023-01-04 11:13:46 +0300

    changes

--- forecast.js
+++ forecast.js
@@ -1,26 +1,26 @@
 const key = 'jGZzj7WPkrvLuraokNYEmvhNGzQzYL94'
 // get weather information
 const getWeather = async (id) => {
-    const base = ' http://dataservice.accuweather.com/currentconditions/v1/';
-    const query =`${id}?apikey= ${key}`;
-    const response = await fetch(base + query );
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+  
+    const response = await fetch(base + query);
     const data = await response.json();
+  
     return data[0];
-
-};
-
-// get city information
-const getCity = async (city) => {
+  
+  };
+  
+  // get city information
+  const getCity = async (city) => {
+  
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
-    const response = await fetch(base + query )
+  
+    const response = await fetch(base + query);
     const data = await response.json();
-     return (data[0]);
-
-
-};
-getCity('manchester').then (data =>{
-    return getWeather(data.Key)
-
-}).then(data)
-.catch(err => console.log(err));
+  
+    return data[0];
+  
+  };
